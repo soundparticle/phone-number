3bbb15878e9a1da74cbdca1e60c1aff0fbf8c1b9
Antreo Pukay <acrosswalker@yahoo.com>
2019-02-18 14:49:46 -0800

    stripped number and now getting it to return

--- phone-number.js
+++ phone-number.js
@@ -1,12 +1,17 @@
-class PhoneNumber  {
-    constructor() {
-        this.number = number;
+class PhoneNumber {
+    
+    constructor(phone) {
+        this.phone = phone;
     }
     
     number() {
-
+        console.log('* in number() *', this.phone.replace(/[^\d]/g, ''));
+        return this.phone;
     }
+    
 };
+// console.log('* return *', this.phone);
+// console.log('* return *', phone.number());
 
 module.exports = {
     PhoneNumber
