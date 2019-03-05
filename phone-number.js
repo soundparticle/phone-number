98af9b3a8c12e893b7d1d2578d402e009a78db67
Antreo Pukay <acrosswalker@yahoo.com>
2019-03-04 17:22:32 -0800

    all tests pass

--- phone-number.js
+++ phone-number.js
@@ -5,34 +5,44 @@ class PhoneNumber {
     
     number() {
         let cleaned = this.phone.replace(/[^\d]/g, '');
-        // console.log('* in number() *', ((cleaned.substring(1).startsWith('0', 1) || cleaned.substring(1).startsWith('1', 1) || cleaned.substring(1).startsWith('2', 1)) === true));   
-        // console.log('* substring  starts *', cleaned.substring(1).startsWith('0') || cleaned.substring(1).startsWith('1') || cleaned.substring(1).startsWith('2'));
-        console.log('* cleaned *', cleaned);   
-        console.log('* substring  starts *', cleaned.startsWith('0', 1));
-        // if(cleaned.length === 9) return null;
+        let firstNum = cleaned.substring(1).charAt(0);
+        let fourthNum = cleaned.substring(1).charAt(3);
         
-        // else if(cleaned.length === 11 && cleaned.startsWith('2'))
-        // return null;
+        console.log('* cleaned *', cleaned.substring(1)); 
+        console.log('firstNum', firstNum);  
+        console.log('fourthdNum', fourthNum);  
+        
+        if(cleaned.length === 9)
+        return null;
+        
+        else if(cleaned.length === 11 && cleaned.startsWith('2'))
+        return null;
 
-        if(cleaned.length === 11 && cleaned.startsWith('1'))
-        return cleaned.substring(1);
+        else if(cleaned.length === 11 && cleaned.startsWith('1')) {
+            if (firstNum < 2 || fourthNum < 2)
+            return null;
+            
+            else return cleaned.substring(1);
+        }
+
+        else if(firstNum < 2 || fourthNum < 2)
+        return null;
         
+        else if(cleaned.length === 12)
+        return null;        
         
-        // else if(cleaned.length === 12) return null;
-        
-        // else if(/[a-z]/i.test(cleaned) === true) return null;
-        
-        if(cleaned.length !== 10) return null;
+        if(cleaned.length !== 10) 
+        return null;
         
         else if(cleaned.startsWith('0') || cleaned.startsWith('1'))
         return null;
         
         else if(cleaned.startsWith('0', 3) || cleaned.startsWith('1', 3))
         return null;
-        
-        else if(cleaned.startsWith('0', 1) || cleaned.startsWith('1', 1) || cleaned.startsWith('0', 4) || cleaned.startsWith('1', 4))
+   
+        else if (firstNum < 2 || fourthNum < 2)
         return null;
-        
+
         else return cleaned;
     }
 };
